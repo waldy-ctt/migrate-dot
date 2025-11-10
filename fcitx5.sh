aa63b8ced2a134a8e251670e8766614c8eb8372f
waldy_ctt <clocktoktok@gmail.com>
2025-11-10 22:31:17 +0700

    add fcitx

---
+++ fcitx5.sh
@@ -0,0 +1,6 @@
+        export XMODIFIERS=@im=fcitx
+        export GTK_IM_MODULE=fcitx
+        export QT_IM_MODULE=fcitx
+        export SDL_IM_MODULE=fcitx
+        # If using Kitty:
+        export GLFW_IM_MODULE=ibus
